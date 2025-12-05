64848c371415ad1a1a993001b097bf703696be74
oklincov <ognjen.klincov@planetsoft.eu>
2025-12-06 00:03:18 +0100

    feat: start the app

---
+++ bank-ok/src/test/java/com/ognjen/bankok/bankok/BankOkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ognjen.bankok.bankok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
